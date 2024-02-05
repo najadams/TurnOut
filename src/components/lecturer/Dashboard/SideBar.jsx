3630078d169c1589c059d0c001fafea17e633de9
najadams <najmadams1706@gmail.com>
2024-02-05 10:13:15 +0000

    some cssing on the files :)

--- src/components/lecturer/Dashboard/SideBar.jsx
+++ src/components/lecturer/Dashboard/SideBar.jsx
@@ -7,7 +7,7 @@ import { logoutLecturer } from "../../../actions/lecturers/lecturerActions";
 const SideBar = ({ logout }) => {
   // Use state to manage the sidebar and dark mode
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  // const [isDarkMode, setIsDarkMode] = useState(false);
 
   // Function to toggle the sidebar
   const toggleSidebar = () => {
