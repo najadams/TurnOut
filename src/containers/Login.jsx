227585b0946e5463d1110d37499e4fc204267732
najadams <najmadams1706@gmail.com>
2024-02-17 15:50:11 +0000

    fixed login page too

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -30,24 +30,30 @@ const Login = ({
   const [referenceId, setReferenceId] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
   // const [classCode, setClassCode] = useState("");
   const [signUp, setSignUp] = useState(false);
 
   const onSubmit = async () => {
     try {
-      if (!referenceId || !password) {
-        setError("Please fill in all fields");
-        return;
-      }
-
       console.log(lecturerErr);
       if (userType == "student") {
-        await handleStudentLogin(referenceId, password, navigate);
+        console.log(firstName,  lastName, referenceId)
+         if (!referenceId || !firstName || !lastName) {
+           setError("Please fill in all fields");
+           return;
+         }
+        await handleStudentLogin(referenceId, firstName, lastName, navigate);
         if (studentErr) {
           setError(studentErr);
           console.log(studentErr);
         }
       } else {
+         if (!referenceId || !password) {
+           setError("Please fill in all fields");
+           return;
+         }
         await handleLecturerLogin(referenceId, password, navigate);
         if (lecturerErr) {
           setError(lecturerErr);
@@ -59,7 +65,7 @@ const Login = ({
   };
 
   useEffect(() => {
-    if (isLoggedIn && userType == 'lecturer') {
+    if (isLoggedIn && userType == "lecturer") {
       navigate("/lecturer/dashboard/");
     }
   }, []);
@@ -79,32 +85,64 @@ const Login = ({
             required
           />
 
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-
-          <button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              onSubmit(); // Invoke the onSubmit function
-            }}>
-            <span>Login</span>
-          </button>
-          {error && <p className="error-message">{error}</p>}
-          {userType == "lecturer" && (
+          {userType === "student" && (
             <>
+              <label htmlFor="firstname">First Name:</label>
+              <input
+                type="text"
+                id="firstname"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+
+              <label htmlFor="lastname">Last Name:</label>
+              <input
+                type="text"
+                id="lastname"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+
+              <button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  onSubmit(); // Invoke the onSubmit function
+                }}>
+                <span>Login</span>
+              </button>
+              {error && <p className="error-message">{error}</p>}
+            </>
+          )}
+
+          {userType === "lecturer" && (
+            <>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+
+              <button
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  onSubmit(); // Invoke the onSubmit function
+                }}>
+                <span>Login</span>
+              </button>
+              {error && <p className="error-message">{error}</p>}
               <input type="checkbox" onClick={() => setSignUp(!signUp)} />
               <p>OR CREATE A NEW ACCOUNT</p>
             </>
           )}
 
-          {userType == "lecturer" && signUp && (
+          {userType === "lecturer" && signUp && (
             <button onClick={() => navigate("/register")}>
               <span>Sign UP</span>
             </button>
@@ -128,10 +166,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleStudentLogin: async (email, password, navigate) => {
+    handleStudentLogin: async (referenceId, firstname, lastname, navigate) => {
       try {
         dispatch(fetchStudentRequest());
-        const user = await loginStudent(email, password);
+        const user = await loginStudent(referenceId, firstname, lastname);
         dispatch(fetchStudentSuccess(user));
         dispatch(loginStudentSuccess());
         console.log("Logged in user:", user);
