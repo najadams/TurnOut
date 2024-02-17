d85a82712301cc42dd971714948f63f8be141e46
najadams <najmadams1706@gmail.com>
2024-02-17 15:36:25 +0000

    rendered all classes the student belongs to on their dashboard

--- src/components/students/StudentDashboard.jsx
+++ src/components/students/StudentDashboard.jsx
@@ -1,14 +1,49 @@
-/**
- * the user sees all the classes he's been to this semester
- * this page navigates to the studentAttendance page
- */
-
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { API_BASE_URL } from "../../containers";
+import { Link } from "react-router-dom";
 
 const StudentDashboard = () => {
+  const [classes, setClasses] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+  const studentInfo = useSelector((state) => state.student);
+  useEffect(() => {
+    const fetchName = async () => {
+      try {
+        const studentId = studentInfo.data.user.studentId;
+        const response = await axios.get(
+          `${API_BASE_URL}/student/${studentId}/classes`
+        );
+        const name = response.data;
+        setClasses(name.classNames);
+        setLoading(false);
+      } catch (error) {
+        console.log("Something went wrong", error);
+        setError(error);
+      }
+    };
+    fetchName();
+  }, [studentInfo.data.user.studentId]);
+
   return (
-    <div className="main">
-      <ScannerPage />
+    <div>
+      <h2 className="Page-name">Student Dashboard</h2>
+      <div>
+        <h2 style={{ paddingBottom: 50 }}>Your Classes</h2>
+        <ul className="dashboard-list">
+          {classes.map((cls, index) => (
+            <Link
+              style={{ textDecoration: "none" }}
+              key={index}
+              to={`class/${cls}`}>
+              <li className="login-form card">{cls}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
