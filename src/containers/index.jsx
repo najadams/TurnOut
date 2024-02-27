a8079d2e783f831ade0b8723ac3fa556c9bf2f7c
najadams <najmadams1706@gmail.com>
2024-02-27 19:06:06 +0000

    added the distance variable

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = "https://mongoserver.cyclic.app/";
 export const API_BASE_URL = "http://localhost:3000";
 export const API_BASE = "localhost:3000";
+export const Dlimit = 100000;
