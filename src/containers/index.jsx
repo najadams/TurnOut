c46b71b5231d2cb7f24978c141cd298efffb81b6
najadams <najmadams1706@gmail.com>
2024-02-28 21:18:36 +0000

    changed url to use online server

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,4 +1,5 @@
-// export const API_BASE_URL = "https://mongoserver.cyclic.app/";
-export const API_BASE_URL = "http://localhost:3000";
-export const API_BASE = "localhost:3000";
+export const API_BASE_URL = "https://mongoserver.cyclic.app/";
+export const API_BASE = "https://mongoserver.cyclic.app/socket";
+// export const API_BASE_URL = "http://localhost:3000";
+// export const API_BASE = "localhost:3000";
 export const Dlimit = 100000;
