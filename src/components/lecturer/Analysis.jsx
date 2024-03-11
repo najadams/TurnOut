b0038f1cc9fbeaeeeb65fa9c90dd98676e41bcfb
najadams <najmadams1706@gmail.com>
2024-03-11 06:57:51 +0000

    wasting time on css

--- src/components/lecturer/Analysis.jsx
+++ src/components/lecturer/Analysis.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import FilterBar from "../../containers/Filter/FilterBar";
+import "./Analysis.css";
 
 const Analysis = () => {
   return (
-    <div>
+    <div style={{ height: "50%" }}>
       <FilterBar />
       <div className="analysis">
         {/* <h2>hello</h2> */}
-        <div className="summary">coco</div>
-        <div className="summary">coco</div>
-        <div className="summary">coco</div>
-        <div className="summary">coco</div>
+        <div className="num_students card-content">num_students</div>
+        <div className="num_attendances card-content">num_attendances</div>
+        <div className="num_classes card-content">num_classes</div>
+        <div className="summary card-content">summary</div>
       </div>
     </div>
   );
