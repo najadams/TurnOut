b4661e28fe37227fb3834d62462be0554d455529
najadams <najmadams1706@gmail.com>
2024-03-14 09:52:40 +0000

    added chart and made it work

--- src/components/lecturer/Analysis.jsx
+++ src/components/lecturer/Analysis.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FilterBar from "../../containers/Filter/FilterBar";
 import "./Analysis.css";
 import { useQuery } from "react-query";
@@ -6,12 +6,16 @@ import { API_BASE_URL } from "../../containers";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import AttendanceChart from "../common/AttendanceChart";
+import RenderTable from "../common/RenderTable";
 
 const Analysis = () => {
   const lecturerId = useSelector(
     (state) => state.lecturer.lecturerInfo.user._id
   );
-
+  const [classes, setClasses] = useState([]);
+  const [isFormSubmitted, setFormSubmitted] = useState(false);
+  // call back function needed to pass data from <Filter />
+  const [chartData, setChartData] = useState(null);
   const fetchClassSummary = async () => {
     const response = await axios.get(
       `${API_BASE_URL}/lecturer/${lecturerId}/summary`
@@ -24,10 +28,18 @@ const Analysis = () => {
     fetchClassSummary
   );
 
+  useEffect(() => {
+    if (data) {
+      const listOfClasses = data.map((coco) => coco.className);
+      setClasses(listOfClasses);
+    }
+  }, [data]);
+
   if (isLoading) return "Loading...";
   if (error) return "An error has occurred: " + error.message;
 
   const numClasses = data.length;
+  const listOfClasses = data.map((coco) => coco.className);
   const numStudents = data.reduce((sum, item) => sum + item.studentCount, 0);
   const numAttendance = data.reduce(
     (sum, item) => sum + item.attendanceCount,
@@ -36,17 +48,21 @@ const Analysis = () => {
 
   return (
     <div className="main-analysis">
-      <FilterBar />
+      <FilterBar
+        classNames={classes}
+        setChartData={setChartData}
+        setFormSubmitted={setFormSubmitted}
+      />
       <div className="analysis">
         <div className="data card-content">
           <div>
-            <h2>num_students :</h2> {numStudents}
+            <h3>num_students :</h3> {numStudents}
           </div>
           <div>
-            <h2>num_attendances :</h2> {numAttendance}
+            <h3>num_attendances :</h3> {numAttendance}
           </div>
           <div>
-            <h2>num_classes : </h2>
+            <h3>num_classes : </h3>
             {numClasses}{" "}
           </div>
         </div>
@@ -56,13 +72,20 @@ const Analysis = () => {
             <h2>summary</h2>
           </div>
           <div className="chart">
-            {data ? (
+            {!isFormSubmitted && data ? (
               <AttendanceChart
                 data={data}
                 chartTitle={"Total classes you manage"}
               />
-            ) : null}
-            {console.log(data)}
+            ) : (
+              <div style={{ width: 500 }} className="table">
+                <RenderTable
+                  data={chartData}
+                  tableName={"Students In Trouble"}
+                  loadingState={"Loading"}
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/containers/Filter/FilterBar.jsx
+++ src/containers/Filter/FilterBar.jsx
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import "./FilterBar.css";
-import { Formik, Form } from "formik";
+import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { API_BASE_URL } from "..";
+import { useSelector } from "react-redux";
 
-const FilterBar = () => {
+const FilterBar = ({ classNames, setChartData, setFormSubmitted }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const initialValues = {
-    className: "",
-    studentName: "",
-    numOfMissOuts: "",
-    attendanceDates: [""],
-  };
+  const lecturerId = useSelector(
+    (state) => state.lecturer.lecturerInfo.user._id
+  );
 
-  const validationSchema = Yup.object({
-    className: Yup.string().required("Required!"),
-    email: Yup.string().email("Invalid Email").required("Required!"),
-    channel: Yup.string().required("Required!"),
-    comments: Yup.string().required("Required!"),
-    address: Yup.string().required("Required!"),
-  });
+ const validationSchema = Yup.object({
+   className: Yup.string().required("Required"),
+   missedAttendance: Yup.number()
+     .min(0, "Must be a positive number")
+     .integer("Must be an integer"),
+   id: Yup.number()
+     .min(100, "Must be a more than 2 digits long")
+     .integer("must be a number"),
+ }).test(
+   "either-or",
+   "Either missedAttendance or id must be filled",
+   (value) => value.missedAttendance || value.id
+ );
+
   const toggleNav = () => {
     setIsOpen(!isOpen);
   };
@@ -31,13 +39,75 @@ const FilterBar = () => {
       </button>
 
       <nav className={`nav fcard ${isOpen ? "open" : "close"}`}>
-        {/* <Formik
-        initialValues={initialValues}
-        >
-          <Form>
+        <h1>My Form</h1>
+        <Formik
+          initialValues={{
+            className: "",
+            missedAttendance: "",
+            id: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={async (values) => {
+            try {
+              // Send a GET request to the endpoint
+              const response = await axios.get(
+                `${API_BASE_URL}/missed/${lecturerId}/${values.className}/${values.missedAttendance}`
+              );
 
-          </Form>
-        </Formik> */}
+              // store the data in the call back function
+              // Pass the data to the parent component
+              setChartData(response.data);
+              setFormSubmitted(true)
+              // Handle the response here. For example, you might want to display the data in an alert:
+              alert(JSON.stringify(response.data, null, 2));
+            } catch (error) {
+              // Handle the error here. For example, you might want to display an error message:
+              alert(`An error occurred: ${error.message}`);
+            }
+          }}>
+          {({ errors, touched, values }) => (
+            <Form>
+              <label htmlFor="className">Class Name</label>
+              <Field as="select" name="className">
+                <option value="">Select a class</option>
+                {classNames.map((className, index) => {
+                  return (
+                    <option value={className} key={index}>
+                      {className}
+                    </option>
+                  );
+                })}
+              </Field>
+              {errors.className && touched.className ? (
+                <div className="error">{errors.className}</div>
+              ) : null}
+
+              <label htmlFor="missedAttendance">
+                Number of Missed Attendance
+              </label>
+              <Field
+                type="number"
+                name="missedAttendance"
+                disabled={!!values.id}
+              />
+              {errors.missedAttendance && touched.missedAttendance ? (
+                <div className="error">{errors.missedAttendance}</div>
+              ) : null}
+
+              <label htmlFor="id">ID</label>
+              <Field
+                type="number"
+                name="id"
+                disabled={!!values.missedAttendance}
+              />
+              {errors.id && touched.id ? (
+                <div className="error">{errors.id}</div>
+              ) : null}
+
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
       </nav>
     </div>
   );
